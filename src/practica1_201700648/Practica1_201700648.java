3529cadcbd3772ddad3438097b1f875cf74a5bc6
Marthinn Chew <Marthinn Chew@DESKTOP-J6VSV4B>
2018-08-21 21:36:47 -0600

    Inicio Practica 1

---
+++ src/practica1_201700648/Practica1_201700648.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_201700648;
+import java.util.Scanner;
+/**
+ *
+ * @author Marthinn Chew
+ */
+public class Practica1_201700648 {                
+
+    public static void main(String[] args) {  
+        int opcion = 0;
+        Scanner entrada = new Scanner(System.in);
+        
+        
+    }
+    
+}
